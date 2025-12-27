3730a7bef5c6316f8d9b35bad35ed9a246e3832e
Arun Sachdev <arunsachdev0112@gmail.com>
2025-12-27 08:44:20 +0000

    404 This page could not be found.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -9,6 +8,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -30,6 +30,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  output: 'export',
 };
 
 export default nextConfig;
